8f3df542490ac0d590573da2196466e6a3afa31d
elia.salerno <elia.salerno@issgreppi.it>
2023-04-26 15:48:46 +0200

    Master: aggiornamento immagini

---
+++ sh_conf/conf.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd /home
+mkdir progetto
+cd progetto
+git init
+
+git config --global user.name "EliaSalerno"
+git config --global user.email "elia.salerno@issgreppi.it"
+ssh-keygen
